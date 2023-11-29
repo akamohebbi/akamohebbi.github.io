8bebe502614d292a15845944fc33f22ce0569a47
akam0098ir <akam0098ir@gmail.com>
2023-11-29 19:44:53 +0330

    working on mobile nav

--- _assets/js/global.js
+++ _assets/js/global.js
@@ -6,26 +6,40 @@ var top_nav_mobile = document.getElementById("top_nav_mobile");
 
 
 var flag_mobile_nav_display = false;
+
+
 btn_display_mobile_nav.addEventListener("click", function() {
     if (!flag_mobile_nav_display) {
         show_top_mobile_nav();
     } else {
         hide_top_mobile_nav();
     }
+    
+    // setTimeout(() => {
+    //     top_nav_mobile_container.classList = "w3-bar animate__animated";        
+    // }, 500);
 });
 
 function show_top_mobile_nav() {
-    top_nav_mobile.style.display = "block";
-    flag_mobile_nav_display = true;
-    top_nav_mobile_container.classList = "w3-bar w3-animate-bottom";
     btn_display_mobile_nav_icon.classList = "fa fa-close";
+    top_nav_mobile.style.display = "block";
+    top_nav_mobile_container.classList.add('animate__fadeInUp');
+    flag_mobile_nav_display = true;
+
+    setTimeout(() => {
+        top_nav_mobile_container.classList.remove('animate__fadeInUp');
+    }, 800);
 }
 
 function hide_top_mobile_nav() {
-    top_nav_mobile.style.display = "none";
     flag_mobile_nav_display = false;
-    top_nav_mobile_container.classList = "w3-bar w3-fading";
+    top_nav_mobile_container.classList.add('animate__fadeOut');
     btn_display_mobile_nav_icon.classList = "fa fa-bars";
+
+    setTimeout(() => {
+        top_nav_mobile.style.display = "none";
+        top_nav_mobile_container.classList.remove('animate__fadeOut');
+    }, 800);
 }
 
 document.addEventListener("scroll", function() {
