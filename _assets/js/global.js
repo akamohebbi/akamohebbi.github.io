6e0b0fcc65696151b7ca72ad8ba7efd6559cb52a
Akam Mohebbi <akamohebbi@gmail.com>
2023-11-30 11:30:47 +0330

    mobile nav bug fixes and better theme color control

--- _assets/js/global.js
+++ _assets/js/global.js
@@ -1,50 +1,51 @@
+---
+---
+
 var btn_display_mobile_nav = document.getElementById("btn_display_mobile_nav");
 var top_nav_mobile_container = document.getElementById("top_nav_mobile_container");
 
-var btn_display_mobile_nav_icon = document.getElementById("btn_display_mobile_nav_icon");
+var btn_display_mobile_nav_icon = document.getElementById("nav-icon4");
 var top_nav_mobile = document.getElementById("top_nav_mobile");
 
 
 var flag_mobile_nav_display = false;
+var flag_is_mobile_nav_animation_finished = false;
 
 
 btn_display_mobile_nav.addEventListener("click", function() {
+    if (flag_is_mobile_nav_animation_finished) return;
+    btn_display_mobile_nav_icon.classList.toggle("open");
+
+
     if (!flag_mobile_nav_display) {
         show_top_mobile_nav();
     } else {
         hide_top_mobile_nav();
     }
-    
-    // setTimeout(() => {
-    //     top_nav_mobile_container.classList = "w3-bar animate__animated";        
-    // }, 500);
 });
 
 function show_top_mobile_nav() {
-    btn_display_mobile_nav_icon.classList = "fa fa-close";
+    flag_is_mobile_nav_animation_finished = true;
+    // btn_display_mobile_nav_icon.classList = "fa fa-close";
     top_nav_mobile.style.display = "block";
-    top_nav_mobile_container.classList.add('animate__fadeInUp');
-    flag_mobile_nav_display = true;
+    top_nav_mobile_container.classList.add('{{ site.top_nav_mobile_enter_animation }}');
 
     setTimeout(() => {
-        top_nav_mobile_container.classList.remove('animate__fadeInUp');
+        top_nav_mobile_container.classList.remove('{{ site.top_nav_mobile_enter_animation }}');
+        flag_mobile_nav_display = true;
+        flag_is_mobile_nav_animation_finished = false;
     }, 800);
 }
 
 function hide_top_mobile_nav() {
-    flag_mobile_nav_display = false;
-    top_nav_mobile_container.classList.add('animate__fadeOut');
-    btn_display_mobile_nav_icon.classList = "fa fa-bars";
+    flag_is_mobile_nav_animation_finished = true;
+    top_nav_mobile_container.classList.add('{{ site.top_nav_mobile_exit_animation }}');
+    // btn_display_mobile_nav_icon.classList = "fa fa-bars";
 
     setTimeout(() => {
         top_nav_mobile.style.display = "none";
-        top_nav_mobile_container.classList.remove('animate__fadeOut');
+        top_nav_mobile_container.classList.remove('{{ site.top_nav_mobile_exit_animation }}');
+        flag_mobile_nav_display = false;
+        flag_is_mobile_nav_animation_finished = false;
     }, 800);
 }
-
-document.addEventListener("scroll", function() {
-    if (flag_mobile_nav_display) {
-        hide_top_mobile_nav();
-        console.log("hi");
-    }
-}); 
